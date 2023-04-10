1261aa4a8f05a102d271d257fa050dc326cb48e5
Mehradi <mehradi.info@gmail.com>
2023-04-10 15:55:13 +0200

    Feching data and mix with Translation

---
+++ src/Models/models.ts
@@ -0,0 +1,53 @@
+export interface ApiData {
+  verses: Verse[];
+  pagination: Pagination;
+}
+
+export interface Pagination {
+  per_page: number;
+  current_page: number;
+  next_page: null;
+  total_pages: number;
+  total_records: number;
+}
+
+export interface Verse {
+  id: number;
+  verse_number: number;
+  verse_key: string;
+  hizb_number: number;
+  rub_el_hizb_number: number;
+  ruku_number: number;
+  manzil_number: number;
+  sajdah_number: null;
+  page_number: number;
+  juz_number: number;
+  words: Word[];
+}
+
+export interface Word {
+  id: number;
+  position: number;
+  audio_url: null | string;
+  char_type_name: CharTypeName;
+  code_v1: string;
+  page_number: number;
+  line_number: number;
+  text: string;
+  translation: Translation;
+  transliteration: Translation;
+}
+
+export enum CharTypeName {
+  End = 'end',
+  Word = 'word',
+}
+
+export interface Translation {
+  text: null | string;
+  language_name: LanguageName;
+}
+
+export enum LanguageName {
+  English = 'english',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,83 @@
-import fs from "fs";
-import path from "path";
-import axios from "axios";
+import fs from 'fs';
+import path from 'path';
+import axios from 'axios';
+import { Verse } from './Models/models';
+import Translation from './data/Translation.json';
+
+interface Quran {
+  id: number;
+  verse: number;
+  chapter: number;
+  key: string;
+  hizb: number;
+  rub: number;
+  ruku: number;
+  manzil: number;
+  sajdah: number | null;
+  page: number;
+  juz: number;
+  text: string;
+  translation: {
+    text: string;
+    iso_code: string;
+  };
+}
+
+const fetchLegacyTranslation = (chapter: number, id: string) => {
+  let t: any = Translation;
+  let data = t[chapter - 1][id]['ayah']['text'];
+  return data;
+};
+
+const getModel = (v: Verse) => {
+  let result: Quran = {
+    id: v.id,
+    key: v.verse_key,
+    verse: v.verse_number,
+    chapter: parseInt(v.verse_key.split(':')[0]),
+    hizb: v.hizb_number,
+    rub: v.rub_el_hizb_number,
+    ruku: v.ruku_number,
+    manzil: v.manzil_number,
+    sajdah: v.sajdah_number,
+    page: v.page_number,
+    juz: v.juz_number,
+    text: v.words.map((w) => w.text).join(''),
+    translation: {
+      iso_code: 'fa',
+      text: fetchLegacyTranslation(
+        parseInt(v.verse_key.split(':')[0]),
+        v.id.toString(),
+      ),
+    },
+  };
+
+  return result;
+};
 
 const fetchSurah = async (index: number) => {
   const response = await axios.get(
-    `https://api.quran.com/api/v4/verses/by_chapter/${index}?language=en&words=true&page=1&per_page=9999`
+    `https://api.quran.com/api/v4/verses/by_chapter/${index}?language=en&words=true&page=1&per_page=9999`,
   );
-  return JSON.stringify(response.data);
+  return await response.data['verses'];
 };
-const genFile = async (index: number, dir: string) => {
-  fs.writeFileSync(path.join(dir, `${index}.json`), await fetchSurah(index));
-};
+// const genFile = async (index: number, dir: string) => {
+//   fs.writeFileSync(path.join(dir, `${index}.json`), await fetchSurah(index));
+// };
 
 const main = async () => {
-  const dir = path.join(__dirname, "files");
+  const resultArr: Quran[] = [];
+  const dir = path.join(__dirname, 'files');
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
-  for (let i = 1; i < 115; i++) {
-    await genFile(i, dir);
+  for (let i = 113; i < 115; i++) {
+    // await genFile(i, dir);
+    (await fetchSurah(i)).map((v: Verse) => {
+      resultArr.push(getModel(v));
+    });
   }
+  fs.writeFileSync(path.join(dir, 'All.json'), JSON.stringify(resultArr));
   return true;
 };
 
